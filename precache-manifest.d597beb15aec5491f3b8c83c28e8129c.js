4b17111003fc5d346d21687ab8839b30eb40ed24
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-03-13 13:35:29 +0530

    Updates

---
+++ precache-manifest.d597beb15aec5491f3b8c83c28e8129c.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6a6de160dad65c91063552362413c0",
+    "url": "/kisaan_network_challenge/index.html"
+  },
+  {
+    "revision": "96ad400ade30aa52bac0",
+    "url": "/kisaan_network_challenge/static/css/8.1594e46e.chunk.css"
+  },
+  {
+    "revision": "67891e86b0c3c5d01144",
+    "url": "/kisaan_network_challenge/static/css/main.35f1f93e.chunk.css"
+  },
+  {
+    "revision": "33409772fec2fe67f10d",
+    "url": "/kisaan_network_challenge/static/js/0.abddb6cb.chunk.js"
+  },
+  {
+    "revision": "400c98ec9fdc207204c2",
+    "url": "/kisaan_network_challenge/static/js/1.fd0d2e11.chunk.js"
+  },
+  {
+    "revision": "d97f6c8fb72575570c39",
+    "url": "/kisaan_network_challenge/static/js/10.5308b129.chunk.js"
+  },
+  {
+    "revision": "083a12ab47da2b734978",
+    "url": "/kisaan_network_challenge/static/js/11.3d17d05d.chunk.js"
+  },
+  {
+    "revision": "491b1fa841e223ea5a17",
+    "url": "/kisaan_network_challenge/static/js/12.c950b4cd.chunk.js"
+  },
+  {
+    "revision": "b0cd0dbb0c10a5643158",
+    "url": "/kisaan_network_challenge/static/js/13.fc03f3f7.chunk.js"
+  },
+  {
+    "revision": "9aff68fdc5aa47b1a83d",
+    "url": "/kisaan_network_challenge/static/js/14.e4b0c701.chunk.js"
+  },
+  {
+    "revision": "6b7e1c90690e0f3276b6",
+    "url": "/kisaan_network_challenge/static/js/15.34f68bfb.chunk.js"
+  },
+  {
+    "revision": "59c87f95d3e0f38d7ea9",
+    "url": "/kisaan_network_challenge/static/js/16.8b75e0ac.chunk.js"
+  },
+  {
+    "revision": "18428fa4bad46eac82c2",
+    "url": "/kisaan_network_challenge/static/js/17.53e57707.chunk.js"
+  },
+  {
+    "revision": "bdb4b8dd6d2da4129f1b",
+    "url": "/kisaan_network_challenge/static/js/18.50bc5891.chunk.js"
+  },
+  {
+    "revision": "f09fc9d21e7472d83a26",
+    "url": "/kisaan_network_challenge/static/js/19.4fb5b60b.chunk.js"
+  },
+  {
+    "revision": "513a68ee8ac4bf28c944",
+    "url": "/kisaan_network_challenge/static/js/2.4e89e184.chunk.js"
+  },
+  {
+    "revision": "365ce6b9eb33bfb931a7",
+    "url": "/kisaan_network_challenge/static/js/20.12083f62.chunk.js"
+  },
+  {
+    "revision": "0bfbc0838f40137d235c",
+    "url": "/kisaan_network_challenge/static/js/21.5dc437f9.chunk.js"
+  },
+  {
+    "revision": "df9b8b410fb614630688",
+    "url": "/kisaan_network_challenge/static/js/22.6db9e7fd.chunk.js"
+  },
+  {
+    "revision": "b16827a30e5a65443661",
+    "url": "/kisaan_network_challenge/static/js/23.f79c4023.chunk.js"
+  },
+  {
+    "revision": "ace611ef76cd280120a4",
+    "url": "/kisaan_network_challenge/static/js/24.89ef8348.chunk.js"
+  },
+  {
+    "revision": "732a3e586faaac4c1198",
+    "url": "/kisaan_network_challenge/static/js/25.d26542c0.chunk.js"
+  },
+  {
+    "revision": "b29325e349803393dad6",
+    "url": "/kisaan_network_challenge/static/js/26.134b0304.chunk.js"
+  },
+  {
+    "revision": "852db0fdfc3c31b613c5",
+    "url": "/kisaan_network_challenge/static/js/27.17f2dc2f.chunk.js"
+  },
+  {
+    "revision": "6e6c52caff8c4abbc8ca",
+    "url": "/kisaan_network_challenge/static/js/28.85b2aac7.chunk.js"
+  },
+  {
+    "revision": "215dc699ca3e288f7d76",
+    "url": "/kisaan_network_challenge/static/js/29.d3e329f7.chunk.js"
+  },
+  {
+    "revision": "b871f3be09f6a9c4cb85",
+    "url": "/kisaan_network_challenge/static/js/3.c035e74d.chunk.js"
+  },
+  {
+    "revision": "cd57b7fb3dcefb3b2fe5",
+    "url": "/kisaan_network_challenge/static/js/30.c58e268e.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/kisaan_network_challenge/static/js/30.c58e268e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ef024219a2c6ae58de",
+    "url": "/kisaan_network_challenge/static/js/31.4c747d42.chunk.js"
+  },
+  {
+    "revision": "dfbe1cd0755833436b39",
+    "url": "/kisaan_network_challenge/static/js/32.b6155f05.chunk.js"
+  },
+  {
+    "revision": "0edcc88548e96b976f99",
+    "url": "/kisaan_network_challenge/static/js/33.8b21de6c.chunk.js"
+  },
+  {
+    "revision": "a0cca47e2238ecb5b41f",
+    "url": "/kisaan_network_challenge/static/js/34.c64ab685.chunk.js"
+  },
+  {
+    "revision": "7c3846d350d11af932b5",
+    "url": "/kisaan_network_challenge/static/js/35.9d456ff5.chunk.js"
+  },
+  {
+    "revision": "98a6cd405cf93e8576ee",
+    "url": "/kisaan_network_challenge/static/js/36.66f41898.chunk.js"
+  },
+  {
+    "revision": "65ceb93e53e2dacb5016",
+    "url": "/kisaan_network_challenge/static/js/37.70d74268.chunk.js"
+  },
+  {
+    "revision": "8d4708986a99b1cdd33f",
+    "url": "/kisaan_network_challenge/static/js/38.6e4f68af.chunk.js"
+  },
+  {
+    "revision": "d6026fa77d885b4c67e3",
+    "url": "/kisaan_network_challenge/static/js/39.44d49c1b.chunk.js"
+  },
+  {
+    "revision": "b0d791d2025b3e3ed0d2",
+    "url": "/kisaan_network_challenge/static/js/4.5d18d73d.chunk.js"
+  },
+  {
+    "revision": "aa3410340de3aa296995",
+    "url": "/kisaan_network_challenge/static/js/40.0b96dfb5.chunk.js"
+  },
+  {
+    "revision": "c0bbee48cddba80c64a7",
+    "url": "/kisaan_network_challenge/static/js/41.5be7bb42.chunk.js"
+  },
+  {
+    "revision": "6d53da6dc49c0829ffec",
+    "url": "/kisaan_network_challenge/static/js/42.c7832982.chunk.js"
+  },
+  {
+    "revision": "d105914cfd0f70dc5ccf",
+    "url": "/kisaan_network_challenge/static/js/43.264b9870.chunk.js"
+  },
+  {
+    "revision": "296d2534e90e6145ddbf",
+    "url": "/kisaan_network_challenge/static/js/44.4178675e.chunk.js"
+  },
+  {
+    "revision": "18516dff7c836a589e7d",
+    "url": "/kisaan_network_challenge/static/js/45.c7fea5a2.chunk.js"
+  },
+  {
+    "revision": "03224c98df57b3d1aab7",
+    "url": "/kisaan_network_challenge/static/js/46.f3ee4d45.chunk.js"
+  },
+  {
+    "revision": "c775aa648c2bd3e717ee",
+    "url": "/kisaan_network_challenge/static/js/47.322c90bf.chunk.js"
+  },
+  {
+    "revision": "3cff203e218826d4f74b",
+    "url": "/kisaan_network_challenge/static/js/48.3f7c64f4.chunk.js"
+  },
+  {
+    "revision": "f769e1b4d4066e154cc3",
+    "url": "/kisaan_network_challenge/static/js/49.f0037819.chunk.js"
+  },
+  {
+    "revision": "c3db35a5657ff9679939",
+    "url": "/kisaan_network_challenge/static/js/5.16052177.chunk.js"
+  },
+  {
+    "revision": "f3de428214053b6629c3",
+    "url": "/kisaan_network_challenge/static/js/50.e85d763d.chunk.js"
+  },
+  {
+    "revision": "7f751d437e5bafb08499",
+    "url": "/kisaan_network_challenge/static/js/51.05c987b1.chunk.js"
+  },
+  {
+    "revision": "d68c21b2b3f7a4719e7f",
+    "url": "/kisaan_network_challenge/static/js/52.84d0c7c6.chunk.js"
+  },
+  {
+    "revision": "64db50b3d32200d42ace",
+    "url": "/kisaan_network_challenge/static/js/53.77f322d4.chunk.js"
+  },
+  {
+    "revision": "5fd81634b50dee1cd42e",
+    "url": "/kisaan_network_challenge/static/js/54.42ad65a9.chunk.js"
+  },
+  {
+    "revision": "dc3b516176635c860ffb",
+    "url": "/kisaan_network_challenge/static/js/55.66636b9d.chunk.js"
+  },
+  {
+    "revision": "6acbebc0aa478ba547fd",
+    "url": "/kisaan_network_challenge/static/js/56.f6f6194b.chunk.js"
+  },
+  {
+    "revision": "dd2be2087d5855c44a80",
+    "url": "/kisaan_network_challenge/static/js/57.41090038.chunk.js"
+  },
+  {
+    "revision": "b9f625484a8ede89fa6a",
+    "url": "/kisaan_network_challenge/static/js/58.e91f34b8.chunk.js"
+  },
+  {
+    "revision": "9230a4047842acd90990",
+    "url": "/kisaan_network_challenge/static/js/59.5ba9a49d.chunk.js"
+  },
+  {
+    "revision": "47b9f9cdf64f3e1cf1cd",
+    "url": "/kisaan_network_challenge/static/js/60.07738f45.chunk.js"
+  },
+  {
+    "revision": "d516374dd19713413f2c",
+    "url": "/kisaan_network_challenge/static/js/61.06f0adb7.chunk.js"
+  },
+  {
+    "revision": "b4f7fd62399e064b069a",
+    "url": "/kisaan_network_challenge/static/js/62.f7832f89.chunk.js"
+  },
+  {
+    "revision": "8d388fb09c432e311626",
+    "url": "/kisaan_network_challenge/static/js/63.60ef17d5.chunk.js"
+  },
+  {
+    "revision": "ffd7f7046efcac3a7460",
+    "url": "/kisaan_network_challenge/static/js/64.1ec65d23.chunk.js"
+  },
+  {
+    "revision": "b6113974f7abcd309b98",
+    "url": "/kisaan_network_challenge/static/js/65.4bed50b9.chunk.js"
+  },
+  {
+    "revision": "1d1a3fb0ad947905bb9d",
+    "url": "/kisaan_network_challenge/static/js/66.28d5156b.chunk.js"
+  },
+  {
+    "revision": "31f11685f1c19c8b427c",
+    "url": "/kisaan_network_challenge/static/js/67.2866fe67.chunk.js"
+  },
+  {
+    "revision": "84deaab76d6b368ce4d9",
+    "url": "/kisaan_network_challenge/static/js/68.3cef5cef.chunk.js"
+  },
+  {
+    "revision": "9384e8497a9d757cc173",
+    "url": "/kisaan_network_challenge/static/js/69.c232f2c8.chunk.js"
+  },
+  {
+    "revision": "e3e272b2d7150670ce25",
+    "url": "/kisaan_network_challenge/static/js/70.5a34c59f.chunk.js"
+  },
+  {
+    "revision": "a1776d4e57e9e9f2f82f",
+    "url": "/kisaan_network_challenge/static/js/71.d2bce3a0.chunk.js"
+  },
+  {
+    "revision": "59de2669ffb0453d3271",
+    "url": "/kisaan_network_challenge/static/js/72.5f3d2f30.chunk.js"
+  },
+  {
+    "revision": "1ae029d2d2d598262b4c",
+    "url": "/kisaan_network_challenge/static/js/73.b50e72a3.chunk.js"
+  },
+  {
+    "revision": "3faed52e70e0e2d65eae",
+    "url": "/kisaan_network_challenge/static/js/74.9c206415.chunk.js"
+  },
+  {
+    "revision": "d1db07ceb50703731df7",
+    "url": "/kisaan_network_challenge/static/js/75.a7ec4fa6.chunk.js"
+  },
+  {
+    "revision": "33cb036122373961ee55",
+    "url": "/kisaan_network_challenge/static/js/76.c7f28b8a.chunk.js"
+  },
+  {
+    "revision": "77111038e6ac2ecc9ec6",
+    "url": "/kisaan_network_challenge/static/js/77.d1b0e983.chunk.js"
+  },
+  {
+    "revision": "cc875abb066da27a1845",
+    "url": "/kisaan_network_challenge/static/js/78.9ebff9ef.chunk.js"
+  },
+  {
+    "revision": "79d9c11566114e0e45c6",
+    "url": "/kisaan_network_challenge/static/js/79.c341cd2d.chunk.js"
+  },
+  {
+    "revision": "96ad400ade30aa52bac0",
+    "url": "/kisaan_network_challenge/static/js/8.9dbf9188.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/kisaan_network_challenge/static/js/8.9dbf9188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680b1efaa63a387c455b",
+    "url": "/kisaan_network_challenge/static/js/80.03aae6b4.chunk.js"
+  },
+  {
+    "revision": "2143015714733ffd67fb",
+    "url": "/kisaan_network_challenge/static/js/81.a993ef0a.chunk.js"
+  },
+  {
+    "revision": "123a9541e6c1d54449d4",
+    "url": "/kisaan_network_challenge/static/js/82.57df3a23.chunk.js"
+  },
+  {
+    "revision": "9b9cc7b26cdb59a56dff",
+    "url": "/kisaan_network_challenge/static/js/83.cbafd597.chunk.js"
+  },
+  {
+    "revision": "6747ee3637f3af3163c1",
+    "url": "/kisaan_network_challenge/static/js/84.12d4ae0c.chunk.js"
+  },
+  {
+    "revision": "9dbc7e5c31c59618c389",
+    "url": "/kisaan_network_challenge/static/js/85.15167fc8.chunk.js"
+  },
+  {
+    "revision": "c4cad0b3a394b11b7601",
+    "url": "/kisaan_network_challenge/static/js/86.2a2217f9.chunk.js"
+  },
+  {
+    "revision": "5027ccaa54dee15b5ed5",
+    "url": "/kisaan_network_challenge/static/js/87.5b1a6ad2.chunk.js"
+  },
+  {
+    "revision": "6f1b8776bf9fe96bd583",
+    "url": "/kisaan_network_challenge/static/js/88.ba0362d8.chunk.js"
+  },
+  {
+    "revision": "c70486cc910821eb6223",
+    "url": "/kisaan_network_challenge/static/js/89.0edaa956.chunk.js"
+  },
+  {
+    "revision": "5adfb148d3e2c2389f32",
+    "url": "/kisaan_network_challenge/static/js/9.ed3dfb40.chunk.js"
+  },
+  {
+    "revision": "7186354dfdc936080d31",
+    "url": "/kisaan_network_challenge/static/js/90.e65eb0b9.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/kisaan_network_challenge/static/js/90.e65eb0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e045519153fb2c0a2ee2",
+    "url": "/kisaan_network_challenge/static/js/91.a5026f73.chunk.js"
+  },
+  {
+    "revision": "f763eb27fe7bc9f23c44",
+    "url": "/kisaan_network_challenge/static/js/92.ad557ddb.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/kisaan_network_challenge/static/js/92.ad557ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7bd5fbb051e54dc5c5",
+    "url": "/kisaan_network_challenge/static/js/93.6b63b99f.chunk.js"
+  },
+  {
+    "revision": "ce18117000c376ed0c73",
+    "url": "/kisaan_network_challenge/static/js/94.8e3ca77f.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/kisaan_network_challenge/static/js/94.8e3ca77f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67891e86b0c3c5d01144",
+    "url": "/kisaan_network_challenge/static/js/main.30103cb9.chunk.js"
+  },
+  {
+    "revision": "8d8c4071a4a33c1b2a98",
+    "url": "/kisaan_network_challenge/static/js/runtime-main.434ced22.js"
+  }
+]);
---
+++ static/js/3.c035e74d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpkisaan_network_challenge=this.webpackJsonpkisaan_network_challenge||[]).push([[3],{169:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(29),i=(e(9),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var a=i.closest("ion-content");a&&a.componentOnReady().then((function(){Object(o.l)((function(){return a.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=3.c035e74d.chunk.js.map
